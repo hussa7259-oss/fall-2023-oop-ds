383081c911c396e313f558a1d20003db814fc626
hussain <hussa7259@gmail.com>
2026-03-11 00:56:42 +0500

    lab 3lab work

---
+++ LAB_03 LabWork_3.cpp
@@ -0,0 +1,22 @@
+// Name: hussain
+// Department: Electrical Engineering (Communication)
+// Registration No: BF25NWELE0666
+// Section: B
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;  // variable to store number
+
+    cout << "Enter a number: ";
+    cin >> num;  // read number from user
+
+    if (num > 0)  // check if number is positive
+        cout << "The number is positive." << endl;
+    else
+        cout << "The number is not positive." << endl;
+
+    return 0;  // program ends
+}
