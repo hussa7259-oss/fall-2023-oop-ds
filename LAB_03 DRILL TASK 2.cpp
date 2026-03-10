3f8c81ac9877ecc6a957d97369c849ea1361a056
hussain <hussa7259@gmail.com>
2026-03-11 00:55:31 +0500

    lab 3 tasks

---
+++ LAB_03 DRILL TASK 2.cpp
@@ -0,0 +1,25 @@
+// Name:hussain 
+// Department: Electrical Engineering (Communication)
+// Registration No: BF25NWELE0666
+// Section: B
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float radius = 0, circumference = 1;  // initialize radius and circumference
+
+    cout << "Enter radius of the circle: ";
+    cin >> radius;  // take input from user
+
+    circumference = 2 * 3.14159 * radius;  // calculate circumference
+
+    // display radius and circumference
+    cout << "Radius = " << radius << endl;
+    cout << "Circumference = " << circumference << endl;
+
+    return 0;  // program ends
+}
+
+
