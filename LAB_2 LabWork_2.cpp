f446ea551cd16b5907f6a864c6115f7bb7eb736a
hussain <hussa7259@gmail.com>
2026-03-11 00:38:41 +0500

    lab work

---
+++ LAB_2 LabWork_2.cpp
@@ -0,0 +1,15 @@
+// Name: hussain
+// Department: Electrical Engineering (Communication)
+// Registration No: BF25NWELE0666
+// Section: B
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    float pi = 3.14;  // declare float variable pi
+    cout << "Value of pi = " << pi << endl;  // display the value
+    return 0;
+}
+
+
---
+++ LAB_2 LabWork_3.cpp
@@ -0,0 +1,21 @@
+// Name: hussain
+// Department: Electrical Engineering (Communication)
+// Registration No: BF25NWELE0666
+// Section: B
+#include <iostream>
+using namespace std;
+
+int main() {
+    float celsius, fahrenheit;  // declare variables
+
+    cout << "Enter temperature in Celsius: ";
+    cin >> celsius;  // take input from user
+
+    // Conversion formula
+    fahrenheit = (celsius * 9 / 5) + 32;
+
+    cout << "Temperature in Fahrenheit: " << fahrenheit << endl;
+
+    return 0;
+}
+
